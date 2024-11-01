865a09f04bd530562f5e4806cc0d468fec7f2568
Skive1 <lugiabao1410@gmail.com>
2024-11-02 01:17:42 +0700

    fix

--- src/java/florastore/filter/DispatcherFilter.java
+++ src/java/florastore/filter/DispatcherFilter.java
@@ -77,6 +77,10 @@ public class DispatcherFilter implements Filter {
         seller.add("sellerUpdateEventPage");
         seller.add("sellerUpdateEvent");
         seller.add("sellerManageEventProductPage");
+        seller.add("orderDeliveringPage");
+        seller.add("sellerCancelOrder");
+        seller.add("sellerViewOrders");
+        seller.add("sellerOrderInformation");
         //Delivery
         delivery = new ArrayList<>();
         delivery.add("deliveryIncome");
